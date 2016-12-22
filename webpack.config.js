4abee24998d5fd7525ee4ac3668098b398d25c97
DESKTOP-RIRO7PA\god <sertion@innorix.com>
2016-12-22 11:25:27 +0900

    custom

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 var webpack = require('webpack');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var BannerPlugin = require("webpack/lib/BannerPlugin");
 var path = require('path');
 var env = require('yargs').argv.mode;
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 var libraryName = 'Library';
 
-var plugins = [], outputFile;
+var plugins = [
+  new BannerPlugin('sertion@innorix.com'),
+  // new BundleAnalyzerPlugin()
+], outputFile;
 
 if (env === 'build') {
   plugins.push(new UglifyJsPlugin({ minimize: true }));
@@ -18,7 +23,7 @@ var config = {
   entry: __dirname + '/src/index.js',
   devtool: 'source-map',
   output: {
-    path: __dirname + '/lib',
+    path: __dirname + '/dist',
     filename: outputFile,
     library: libraryName,
     libraryTarget: 'umd',
